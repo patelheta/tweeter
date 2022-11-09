9c6eea7568a6859889fdd14a588c5114a362f8cf
patelheta <patelheta385@gmail.com>
2022-11-08 17:12:48 -0800

    implement text-area word counter by using jQuery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function() {
+    let text = $(this).val();
+    let limit = 140 - text.length;
+    let counterLabel = $(this).parents().find('.counter');
+    counterLabel.val(limit);
+    if (limit < 0) {
+      counterLabel.css("color", "red");
+    }
+    else {
+      counterLabel.css("color", "#55514a");
+    }
+  });
+
+
+
+
+});
